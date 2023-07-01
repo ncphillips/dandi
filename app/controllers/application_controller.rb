bfc5512d729d67d4356abbde04373245715ebaf5
nolan <nolan@ncphi.com>
2023-06-30 21:28:47 -0300

    standardrb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found 
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
   private
 
